965da29ba97a847ec0a181708bbbabf2365510b6
rubensrodrigues <rubens.rodrigues@firstdecision.com.br>
2017-10-04 13:23:42 -0300

    first commit

---
+++ src/main/java/com/br/minhadispensa/minhadispensa/MinhadispensaApplication.java
@@ -0,0 +1,12 @@
+package com.br.minhadispensa.minhadispensa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhadispensaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhadispensaApplication.class, args);
+	}
+}
